03f39399199a21c7eae5c6cbeca8b7403c1d8395
caperaven <caperaven@gmail.com>
2022-08-21 01:24:07 +0200

    util for getting value on path

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,12 +7,19 @@ mod utils;
 use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
+/**
+    Bindings for debugging
+**/
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn console_log(s: &str);
 }
 
+/**
+    Utility function to get a value on a object path
+    Exposed for testing purposes
+**/
 #[wasm_bindgen]
 pub fn get_value(obj: &JsValue, path: &str) -> JsValue {
     match utils::get_value(obj, path) {
@@ -21,11 +28,20 @@ pub fn get_value(obj: &JsValue, path: &str) -> JsValue {
     }
 }
 
+/**
+    Check if a object matches the filter intent.
+    Based on the filter intent, return true if the object passes evaluation.
+    If the object is excluded by the evaluation it returns false.
+**/
 #[wasm_bindgen]
 pub fn in_filter(intent: &JsValue, row: &JsValue, case_sensitive: bool) -> Result<bool, JsValue> {
     evaluators::evaluate_object(intent, row, case_sensitive)
 }
 
+/**
+    Given an array of objects execute the filter and return an array of indexes of the items that
+    passes the filter criteria
+**/
 #[wasm_bindgen]
 pub fn filter(data: &Array, intent: &JsValue, case_sensitive: bool) -> Result<Array, JsValue> {
     let result = Array::new();
@@ -46,6 +62,11 @@ pub fn filter(data: &Array, intent: &JsValue, case_sensitive: bool) -> Result<Ar
     Ok(result)
 }
 
+/**
+    Sort the array of objects based on the sort intent.
+    If you only want to sort a subset of the records, pass in an array of indexes for the objects
+    that must make up the sort result.
+**/
 #[wasm_bindgen]
 pub fn sort(data: &Array, intent: &JsValue, rows: Option<Vec<usize>>) -> Result<Vec<usize>, JsValue> {
     let result: Result<Vec<usize>, JsValue> = match rows {
