fbd6dcdeb239cb70cce1780797e97ce2c59ac3b9
caperaven <caperaven@gmail.com>
2022-10-09 21:01:07 +0200

    code cleanup

--- tests/unique_values.test.js
+++ tests/unique_values.test.js
@@ -5,9 +5,10 @@ await init();
 init_panic_hook();
 
 Deno.test("unique values", () => {
-    const result = unique_values([{value: 1}, {value: 2}, {value: 3}, {value: 3}], ["value"]);
+    const result = unique_values([{value: 1}, {value: 2}, {value: 3}, {value: 3}, {value: null}], ["value"]);
 
     assertEquals(result["value"]["1"], 1);
     assertEquals(result["value"]["2"], 1);
     assertEquals(result["value"]["3"], 2);
+    assertEquals(result["value"]["null"], 1);
 })
